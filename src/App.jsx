539b931648e54bf5869801bbec0221200281c162
Erika Nagaki <enagaki.22@gmail.com>
2023-12-20 19:07:33 -0500

    Part 1.6 unicafe step1

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,25 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+)
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <h1>{course.name}</h1>
-      <br></br>
-      <p>{course.parts[0].name} : {course.parts[0].exercises}</p>
-      <p>{course.parts[1].name} : {course.parts[1].exercises}</p>
-      <p>{course.parts[2].name} : {course.parts[2].exercises}</p>
-      <br></br>
-      <h3>Number of exercises : {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</h3>
+      <h2>give feedback</h2>
+      <Button handleClick= {() => setGood(good + 1)} text="good" />
+      <Button handleClick= {() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick= {() => setBad(bad + 1)} text="bad" />
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
