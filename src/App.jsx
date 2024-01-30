e26bba2f79a552077e4e27179883954ed9421735
Erika Nagaki <enagaki.22@gmail.com>
2024-01-30 18:28:07 -0500

    Part 2.3 course information step8

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,17 @@ const Header = (props) =>{
 }
 
 const Content = (props) => {
+  const partsExcercises = props.course.map(course => course.exercises)
+  const sumTotal = partsExcercises.reduce((sum, excercise) => sum + excercise, 0)
+
+  console.log('Content component', props, props.course, sumTotal)
+
   return(
     <div>
       <Part name={props.course[0].name} exercises={props.course[0].exercises}/>
       <Part name={props.course[1].name} exercises={props.course[1].exercises}/>
       <Part name={props.course[2].name} exercises={props.course[2].exercises}/>
-      <b>total of {props.course[0].exercises + props.course[1].exercises + props.course[2].exercises} exercises</b>
+      <b>total of {sumTotal} exercises</b>
     </div>
   )
 }
