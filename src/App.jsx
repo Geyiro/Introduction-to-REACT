3fc46738ee0053ca9d7fdd5ed0bef40e6559a3a7
Erika Nagaki <enagaki.22@gmail.com>
2024-01-30 17:18:27 -0500

    Part 2.1 course information step6

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,58 @@
-import { useState } from 'react'
-
-const Button = (props) => (
-  <button onClick={props.handleClick}>{props.text}</button>
-)
-
-const anecdotes = [
-  'If it hurts, do it more often.',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-  'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-  'The only way to go fast, is to go well.'
-]
-
-const App = () => {
-  const [selected, setSelected] = useState(0)
-  const [voted, setVoted] = useState({})
-
-  const getRandomInt = () => Math.floor(Math.random() * anecdotes.length)
-
-  const handleVote = (anecdoteIndex) => {
-    const currentVotes = voted[anecdoteIndex]
-    const votesCopy = {...voted}
-
-    if (currentVotes) {
-      votesCopy[anecdoteIndex] += 1
-    } else {
-      votesCopy[anecdoteIndex] = 1
-    }
-
-    setVoted(votesCopy)
-  }
-
-  let maxVotes = 0
-  let maxIndex = -1
-
-  for (const [index, votes] of Object.entries(voted)) {
-    if (votes > maxVotes) {
-      maxVotes = votes
-      maxIndex = index
-    }
-  }
-
+const Course = ({ course }) =>{
   return (
     <div>
-      <h2>Anecdote of the day</h2>
-      <p>{anecdotes[selected]}</p>
-      <p>has {voted[selected] ?? 0} votes</p>
-      <Button handleClick= {() => handleVote(selected)} text="vote"/>
-      <Button handleClick= {() => setSelected(getRandomInt())} text="next anecdote"/>
-      <h2>Anecdote with most votes</h2>
-      <p>{ maxIndex > -1 ? anecdotes[maxIndex] : '' }</p>
-      <p>has {maxVotes} votes</p>
+      <Header name={course.name}/>
+      <Content course={course.parts}/>
     </div>
   )
+}
+
+const Header = (props) =>{
+  return(
+    <h1>{props.name}</h1>
+  )
+}
+
+const Content = (props) => {
+  return(
+    <div>
+      <Part name={props.course[0].name} exercises={props.course[0].exercises}/>
+      <Part name={props.course[1].name} exercises={props.course[1].exercises}/>
+      <Part name={props.course[2].name} exercises={props.course[2].exercises}/>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.name} = {props.exercises}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
